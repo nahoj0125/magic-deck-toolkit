1f91e829a5dff72d506e5022684bdbd227fdb81d
Johan Persson <perssonjohan95@gmail.com>
2025-10-14 15:27:26 +0200

    Add module configuration

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as Card } from './src/Card.js'
+export { default as Deck } from './src/Deck.js'
+export { default as ManaAnalyzer } from './src/ManaAnalyzer.js'
+export { default as CardTypeAnalyzer } from './src/CardTypeAnalyzer.js'
